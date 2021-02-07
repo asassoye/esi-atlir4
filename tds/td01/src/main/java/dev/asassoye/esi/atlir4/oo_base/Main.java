67464d85a0473c3961b3267ff2af1352174e72b2
Andrew SASSOYE <andrew@sassoye.be>
2021-02-07 12:43:31 +0100

    td01: Point and PointTest

--- tds/td01/src/main/java/dev/asassoye/esi/atlir4/oo_base/Main.java
+++ tds/td01/src/main/java/dev/asassoye/esi/atlir4/oo_base/Main.java
@@ -2,6 +2,10 @@ package dev.asassoye.esi.atlir4.oo_base;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        Point point = new Point();
+        System.out.println(point);
+
+        point.move(2, 2);
+        System.out.println(point);
     }
 }
