967bd2a93f5b3878d28c51cd0035b3b58b6acb40
Andrew SASSOYE <andrew@sassoye.be>
2021-02-07 12:12:15 +0100

    create repo structure + maven configuration

---
+++ README.md
@@ -0,0 +1,1 @@
+# [ATLIR4] Atelier Logiciel
---
+++ projects/blackjack/src/main/java/dev/asassoye/esi/atlir4/blackjack/Main.java
@@ -0,0 +1,7 @@
+package dev.asassoye.esi.atlir4.blackjack;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+    }
+}
