a09f21c965e50f5ecf51c6e559200d53243fd552
Andrew SASSOYE <andrew@sassoye.be>
2021-02-23 22:44:09 +0100

    asciipaint: Main application with regex

--- modules/asciipaint/src/main/java/dev/asassoye/esi/atlir4/asciipaint/Application.java
+++ modules/asciipaint/src/main/java/dev/asassoye/esi/atlir4/asciipaint/Application.java
@@ -22,11 +22,27 @@
 
 package dev.asassoye.esi.atlir4.asciipaint;
 
+import java.util.Scanner;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
 /**
  * The type Application.
  */
 public class Application {
-    private AsciiPaint paint;
+    private static final Scanner scanner = new Scanner(System.in);
+
+    private static final Pattern regex = Pattern.compile("^(show|quit|add)( (circle|rectangle|square) (\\d*) (\\d*) (\\d*) ?(\\d*)? (.))?$");
+
+    private final AsciiPaint paint;
+
+    private Application() {
+        this(15, 15);
+    }
+
+    private Application(int width, int height) {
+        this.paint = new AsciiPaint(width, height);
+    }
 
     /**
      * The entry point of application.
@@ -34,17 +50,63 @@ public class Application {
      * @param args the input arguments
      */
     public static void main(String[] args) {
-        AsciiPaint asciiPaint = new AsciiPaint(15, 15);
-        asciiPaint.newCircle(10, 5, 3, 'c');
-        asciiPaint.newSquare(1, 1, 3, 'r');
-
-        System.out.println(asciiPaint.asAscii());
+        Application app = new Application();
+        app.start();
     }
 
     /**
      * Start.
      */
     public void start() {
-        //TODO
+        while (true) {
+            System.out.println("What do you want to do: ");
+            String response = scanner.nextLine();
+
+            Matcher m = regex.matcher(response);
+
+            if (m.find()) {
+                String command = m.group(1);
+                switch (command) {
+                    case "quit":
+                        return;
+                    case "show":
+                        System.out.println(this.paint.asAscii());
+                        break;
+                    case "add":
+                        String shape = m.group(3);
+                        int x, y, radius, height, width;
+                        char color;
+
+                        switch (shape) {
+                            case "circle":
+                                x = Integer.parseInt(m.group(4));
+                                y = Integer.parseInt(m.group(5));
+                                radius = Integer.parseInt(m.group(6));
+                                color = m.group(8).charAt(0);
+                                this.paint.newCircle(x, y, radius, color);
+                                break;
+                            case "rectangle":
+                                x = Integer.parseInt(m.group(4));
+                                y = Integer.parseInt(m.group(5));
+                                width = Integer.parseInt(m.group(6));
+                                height = Integer.parseInt(m.group(7));
+                                color = m.group(8).charAt(0);
+                                this.paint.newRectangle(x, y, width, height, color);
+                                break;
+                            case "square":
+                                x = Integer.parseInt(m.group(4));
+                                y = Integer.parseInt(m.group(5));
+                                width = Integer.parseInt(m.group(6));
+                                height = Integer.parseInt(m.group(6));
+                                color = m.group(8).charAt(0);
+                                this.paint.newRectangle(x, y, width, height, color);
+                                break;
+                        }
+                }
+            } else {
+                System.out.println("Bad command, try again: ");
+            }
+        }
+
     }
 }
