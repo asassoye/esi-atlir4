7c63900304e9cb9f0ab7c56668ad3381935ab1bd
Andrew SASSOYE <andrew@sassoye.be>
2021-08-29 18:25:33 +0200

    fix build

---
+++ modules/bmr/src/main/java/module-info.java
@@ -0,0 +1,7 @@
+module atlir.bmr {
+    requires java.logging;
+    requires java.desktop;
+    requires javafx.controls;
+
+    exports dev.asassoye.esi.atlir4.bmr;
+}
---
+++ modules/fxhello/src/main/java/module-info.java
@@ -0,0 +1,7 @@
+module atlir.fxhello {
+    requires java.logging;
+    requires java.desktop;
+    requires javafx.controls;
+
+    exports dev.asassoye.esi.atlir4.fxhello;
+}
